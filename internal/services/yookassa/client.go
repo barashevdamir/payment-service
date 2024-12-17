0954da2a57140b7cf72762f5cc70fe86a0b565c4
Дамир Барашев <barashev-01@mail.ru>
2024-12-17 17:06:26 +0300

    feat(): added structs

--- internal/services/yookassa/client.go
+++ internal/services/yookassa/client.go
@@ -11,7 +11,7 @@ type endpoint struct {
 	Method string `yaml:"method"`
 }
 
-type yookassaClient struct {
+type YookassaClient struct {
 	Scheme    string `yaml:"scheme"`
 	Host      string `yaml:"host"`
 	SecretKey string
@@ -20,23 +20,111 @@ type yookassaClient struct {
 }
 
 type yookassaEndpoints struct {
-	Payments endpoint `yaml:"payment"`
+	CreatePayment endpoint `yaml:"create_payment"`
+	PaymentsList  endpoint `yaml:"payments_list"`
 }
 
 type Amount struct {
 	Value    float64
-	Currency string "RUB"
+	Currency string
 }
 
 type Confirmation struct {
-	Type      string "redirect"
+	Type      string
 	Locale    string
 	ReturnUrl string
 	Enforce   bool
 }
 
-func NewYookassaClient(scheme, host, secretKey, accountId string, endpoints yookassaEndpoints) *yookassaClient {
-	return &yookassaClient{
+type Recipient struct {
+	AccountId string
+	GatewayId string
+}
+
+type PaymentMethod struct {
+	Type           string
+	Id             string
+	Saved          bool
+	Title          string
+	DiscountAmount Amount
+	LoanOption     string
+}
+
+type CancellationDetails struct {
+	Party  string
+	Reason string
+}
+
+type ThreeDSecure struct {
+	Applied bool
+}
+type AuthorizationDetails struct {
+	RRN          string
+	AuthCode     string
+	ThreeDSecure ThreeDSecure
+}
+
+type Transfer struct {
+	AccountId         string
+	Amount            Amount
+	Status            string
+	PlatformFeeAmount Amount
+	Description       string
+	Metadata          map[string]string
+}
+
+type Settlement struct {
+	Type   string
+	Amount Amount
+}
+
+type Deal struct {
+	Id          string
+	Settlements []Settlement
+}
+
+type InvoiceDetails struct {
+	Id string
+}
+
+type Payment struct {
+	Id                    string
+	Status                string
+	Amount                Amount
+	IncomeAmount          Amount
+	Description           string
+	Recipient             Recipient
+	PaymentMethod         PaymentMethod
+	CapturedAt            string
+	CreatedAt             string
+	ExpiresAt             string
+	Confirmation          Confirmation
+	Test                  bool
+	RefundedAmount        Amount
+	Paid                  bool
+	Refundable            bool
+	RecipientRegistration string
+	Metadata              map[string]string
+	CancellationDetails   CancellationDetails
+	AuthorizationDetails  AuthorizationDetails
+	Transfers             []Transfer
+	Deal                  Deal
+	MerchantCustomerId    string
+	InvoiceDetails        InvoiceDetails
+}
+
+func NewYookassaClient(scheme string, host string, secretKey string, accountId string) *YookassaClient {
+	endpoints := yookassaEndpoints{
+		CreatePayment: endpoint{
+			Path:   "/payments",
+			Method: "POST",
+		},
+		PaymentsList: endpoint{
+			Path:   "/payments",
+			Method: "GET",
+		},
+	}
+	return &YookassaClient{
 		Scheme:    scheme,
 		Host:      host,
 		SecretKey: secretKey,
